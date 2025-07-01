ad4a28d8b57ccc13b3436da60b8eeec5cacd9a8f
murali-karthik01 <muralikarthikedu@gmail.com>
2025-07-01 11:05:30 +0530

    Delete HEALTHAI PROJECT/Model/granite_loader.py

--- HEALTHAI PROJECT/Model/granite_loader.py
+++
@@ -1,8 +0,0 @@
-from transformers import AutoTokenizer, AutoModelForCausalLM, pipeline
-
-def load_granite_model():
-    model_dir = "./granite-3.3-2b-instruct"
-    tokenizer = AutoTokenizer.from_pretrained(model_dir)
-    model = AutoModelForCausalLM.from_pretrained(model_dir)
-    pipe = pipeline("text-generation", model=model, tokenizer=tokenizer)
-    return pipe
